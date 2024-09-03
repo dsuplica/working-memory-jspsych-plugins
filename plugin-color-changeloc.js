6a0fdab08ebaa26fd2452137c67631d8e8422579
Darius Suplica <dsuplica@uchicago.edu>
2024-09-03 13:33:17 -0500

    add in trial procedure

--- plugin-color-changeloc.js
+++ plugin-color-changeloc.js
@@ -6,8 +6,8 @@ function range(start=0,end){
 }
 
 // random number between min and max
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 // euclidean distance
@@ -67,11 +67,21 @@ var jsPsychColorChangeLoc = (function (jspsych) {
           ],
         },
         /** The duration of the stimulus display */
-        stim: {
+        stim_duration: {
           type: jspsych.ParameterType.INT,
           default: 250,
           pretty_name: "Stimulus duration",
+        },
+
+        /** the duration of the delay period (ms) */
+
+        delay_duration: {
+          type: jspsych.ParameterType.INT,
+          default: 4000,
+          pretty_name: "Delay duration",
         }
+
+
       },
     data: {
       /** name of this trial */
@@ -120,6 +130,10 @@ var jsPsychColorChangeLoc = (function (jspsych) {
       n_colors: {
         type: jspsych.ParameterType.INT,
       },
+      /** Accuracy */
+      accuracy: {
+        type: jspsych.ParameterType.BOOL,
+      }
     }
   };
 
@@ -143,6 +157,7 @@ var jsPsychColorChangeLoc = (function (jspsych) {
         let test_index; // index of item to be changed
         let test_item; // new value for changed item
         let response_array = range(1, trial.n_colors + 1); // array of possible responses
+        const valid_responses = response_array.map(String); // array of valid responses
         const canvasSize = 600;
         const edge_buffer = 50;
         const stim_buffer = 10; 
@@ -197,17 +212,18 @@ var jsPsychColorChangeLoc = (function (jspsych) {
 
       let position_array = [];
 
+      const stim_diag = Math.sqrt(2) * stim_size; // diagonal distance
 
       while (position_array.length < trial.n_colors) {
-        let x = randomNumber(edge_buffer, canvasSize - edge_buffer);
-        let y = randomNumber(edge_buffer, canvasSize - edge_buffer);
+        let x = randomInt(edge_buffer, canvasSize - edge_buffer);
+        let y = randomInt(edge_buffer, canvasSize - edge_buffer);
     
         if (dist_between_points(x, y, 0, 0) < stim_size + stim_buffer) {
             continue; // too close to center
         }
         
         for ([x2,y2] of position_array) {
-            if (dist_between_points(x, y, x2, y2) < stim_size + stim_buffer) {
+            if (dist_between_points(x, y, x2, y2) < stim_diag + stim_buffer) {
                 continue; // too close to another stimulus
             }
         }
@@ -221,38 +237,148 @@ var jsPsychColorChangeLoc = (function (jspsych) {
 
     let stimulus_array = [];
     let colors_shuff = shuffleArray(trial.colors);
-    for (let i = 0; i < trial.n_colors; i++) {
+    for (let i = 0; i < trial.n_colors; i++) { // append stimuli (should be already shuffled)
       stimulus_array.push({stimulus: colors_shuff[i], type: "color"});
     }
 
+
     
 
+    // drawing functions, basically straight copy paste
+    const draw_stim = (stimulus, pos, label) => {
+      var pattern = /^#/;
+      var is_color = pattern.test(stimulus); //SEARCH FOR '#' IN STIMULUS ARGUMENT TO CONFIRM STIMULUS IS A COLOR//
+      return new Promise(async (resolve, reject) => {
+        if (is_color) {
+          var rect = new fabric.Rect({
+            width: stim_size,
+            height: stim_size,
+            left: pos[0] - stim_size / 2, // shift to convert from center to left and top
+            top: pos[1] + stim_size / 2,
+            fill: stimulus,
+            hasBorders: false,
+            hasControls: false,
+            hoverCursor: "default",
+            lockMovementX: true,
+            lockMovementY: true,
+          });
+          canvas.add(rect);
+          canvas.requestRenderAll();
+          resolve();
+          if (label) {
+            //ADD NUMBER LABEL//
+            var label_object = new fabric.Text(label.toString(), {
+              left: pos[0] - stim_size / 2, // shift to convert from center to left and top
+              top: pos[1] + stim_size / 2,
+              fill: "#9897A9",
+              fontSize: 30,
+              fontWeight: "bold",
+              hasBorders: false,
+              hasControls: false,
+              hoverCursor: "default",
+              lockMovementX: true,
+              lockMovementY: true,
+            });
+            canvas.add(label_object);
+            canvas.requestRenderAll();
+            resolve();
+          }
+        }
+      });
+    };
+
+    const present_test = async () => {
+      //CREATE ARRAY OF DESIRED RESPONSES & SHUFFLE//
+      let response_array = shuffleArray(response_array);
+      // get index of test item
+      test_index = randomInt(0,stimulus_array.length);
+
+      for (var i = 0; i < stimulus_array.length; i++) {
+        // replace test stim with a new one
+        if (i === test_index) {
+          test_item = colors_shuff[7];
+          draw_stim(test_item, position_array[i], response_array[i]);
+        } else {
+          draw_stim(
+            stimulus_array[i].stimulus,
+            position_array[i],
+            response_array[i]
+          );
+        }
+        canvas.requestRenderAll();
+      }
+
+      // event listener
+      // idk what this does tbh
+      var keyboard_listener = jsPsych.pluginAPI.getKeyboardResponse({
+        callback_function: afterResponse,
+        valid_responses: valid_responses,
+        rt_method: "performance",
+        persist: false,
+        allow_held_key: false,
+      });
+    };
 
 
+    // MAIN TRIAL PROCEDURE HERE
 
+    const trial_procedure = async () => {
 
+      
+      // present initial stimulus array
+      for (let istim = 0; istim < stimulus_array.length; istim++) {
+        await draw_stim(stimulus_array[istim].stimulus, position_array[istim]);
+      }
 
+      // present delay period and then test
+      // i should be an honorary italian
+      // this is straight spaghetti i hate it
+      this.jsPsych.pluginAPI.setTimeout(function () {
+        // function to remove objects, executed at end of stim period
+        canvas.getObjects().forEach((o) => {
+          if (!o.id) {
+            canvas.remove(o);
+          }
+        });
+        canvas.requestRenderAll();
+          this.jsPsych.pluginAPI.setTimeout(function () {
+            test_procedure();
+          }, trial.delay_duration); // delay period, executed at end
+        }, trial.stim_duration);
+      };
 
+      // actually run the trial here
+      show_fixation();
+      this.jsPsych.pluginAPI.setTimeout(trial_procedure, trial.fixation_duration);
 
-
-
-
-
-
-
-
-
+      function afterResponse(info){
+        end_trial(info.key,info.rt)
+      }
+      const end_trial = (key,rt) => {
 
         // data saving
         var trial_data = {
-          data1: 99, // Make sure this type and name matches the information for data1 in the data object contained within the info const.
-          data2: "hello world!", // Make sure this type and name matches the information for data2 in the data object contained within the info const.
+          trial_exp: "color_change_localization",
+          key: key,
+          rt: rt,
+          stimuli: stimulus_array,
+          positions: position_array,
+          test_index: test_index,
+          test_item: test_item,
+          correct_answer: response_array[test_index],
+          response_array: response_array,
+          n_colors: trial.n_colors,
+          accuracy: key == response_array[test_index],
+          
+
         };
-        // end trial
+        console.log(trial_data)
+        // end trial and go to next
         this.jsPsych.finishTrial(trial_data);
       }
     }
-    PluginNamePlugin.info = info;
+  }
+    ColorChangeLocPlugin.info = info;
   
-    return PluginNamePlugin;
+    return ColorChangeLocPlugin;
   })(jsPsychModule);
